a6123dd78df33900551095f81ed7ffa620354512
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-11-23 10:59:51 -0500

    commit tree 2d074e1785f5c51138607f862e34abd03d7ef46b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  attr_accessor :word
+
+  def match(words)
+    letters = @word.split('')
+    compare_letters = letters.sort
+    matches =
+      words.select do |given_word|
+        split_word = given_word.split('')
+        for_comparison = split_word.sort
+        compare_letters == for_comparison
+      end
+    matches
+  end
+end
